1c2ea2f41cd53ee851f92d8d95aed44687959fbd
Lars Jansøn Engvik <lars@engvik.nu>
2020-05-01 13:56:32 +0200

    feat(cmd/reddify): add comments

--- cmd/reddify/main.go
+++ cmd/reddify/main.go
@@ -14,6 +14,7 @@ import (
 )
 
 func main() {
+	// Parse command line arguments into a config struct
 	cfg, err := config.Parse()
 	if err != nil {
 		log.Fatalf("error parsing config: %s", err.Error())
@@ -23,16 +24,19 @@ func main() {
 		log.Printf("reddify %s", cfg.Version)
 	}
 
+	// Set up spotify client
 	s, err := spotify.New(cfg)
 	if err != nil {
-		log.Fatalf("error creating reddit client: %s", err.Error())
+		log.Fatalf("error creating spotify client: %s", err.Error())
 	}
 
+	// Set up reddit client
 	r, err := reddit.New(cfg, s.MusicChan)
 	if err != nil {
 		log.Fatalf("error creating reddit client: %s", err.Error())
 	}
 
+	// Set up a very basic http server to handle auth callbacks
 	http.HandleFunc("/spotifyAuth", s.AuthHandler())
 	go func() {
 		if err := http.ListenAndServe(fmt.Sprintf(":%s", cfg.HTTPPort), nil); err != nil {
@@ -40,6 +44,7 @@ func main() {
 		}
 	}()
 
+	// Authenticate spotify
 	s.Log("awaiting authentication...")
 
 	s.Authenticate()
@@ -47,16 +52,19 @@ func main() {
 
 	s.Log("authenticated!")
 
+	// Prepare the Spotify playlist
 	if err := s.PreparePlaylist(cfg); err != nil {
 		log.Fatalf("error preparing playlist: %s", err.Error())
 	}
 
 	s.Log(fmt.Sprintf("spotify playlist ready: %s (%s)", s.Playlist.Name, s.Playlist.ID))
 
+	// Prepare the reddit scanner
 	if err := r.PrepareScanner(); err != nil {
 		log.Fatalf("error preparing reddit/graw scanner: %s", err.Error())
 	}
 
+	// Start listening and block until shutdown signal receieved
 	func(s *spotify.Client, r *reddit.Client) {
 		shutdown := make(chan os.Signal, 1)
 		signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
