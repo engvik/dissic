cdc2e8af28021e81c8a097ce227a9d542c8528d1
Lars Jansøn Engvik <lars@engvik.nu>
2020-05-13 19:36:58 +0200

    refactor(dissic): separate dissic client from main

--- cmd/dissic/main.go
+++ cmd/dissic/main.go
@@ -5,11 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/engvik/dissic/config"
+	"github.com/engvik/dissic/dissic"
 	"github.com/engvik/dissic/reddit"
 	"github.com/engvik/dissic/spotify"
 )
@@ -42,70 +40,18 @@ func main() {
 	// Set up http server
 	mux := http.NewServeMux()
 	mux.HandleFunc("/spotifyAuth", s.AuthHandler())
-	d := dissic{
-		config:  cfg,
-		spotify: s,
-		reddit:  r,
-		http: &http.Server{
+
+	// Set up dissic client
+	d := dissic.Client{
+		Config:  cfg,
+		Spotify: s,
+		Reddit:  r,
+		HTTP: &http.Server{
 			Addr:    fmt.Sprintf(":%d", cfg.HTTPPort),
 			Handler: mux,
 		},
 	}
 
-	d.start(ctx)
-}
-
-type dissic struct {
-	config  *config.Config
-	spotify *spotify.Client
-	reddit  *reddit.Client
-	http    *http.Server
-}
-
-func (d *dissic) start(ctx context.Context) {
-	go func(s *http.Server) {
-		if err := s.ListenAndServe(); err != nil {
-			log.Fatalf("error starting http server: %s", err.Error())
-		}
-	}(d.http)
-
-	// Authenticate spotify
-	d.spotify.Log("awaiting authentication...")
-	d.spotify.Authenticate()
-	<-d.spotify.AuthChan
-	d.spotify.Log("authenticated!")
-
-	// Get Spotify playlists
-	if err := d.spotify.GetPlaylists(d.config); err != nil {
-		log.Fatalf("error preparing playlists: %s", err.Error())
-	}
-
-	// Prepare the reddit scanner
-	if err := d.reddit.PrepareScanner(); err != nil {
-		log.Fatalf("error preparing reddit/graw scanner: %s", err.Error())
-	}
-
-	// Start listening and block until shutdown signal receieved
-	func(ctx context.Context, d *dissic) {
-		shutdown := make(chan os.Signal, 1)
-		signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
-
-		go d.spotify.Listen()
-		d.spotify.Log("worker ready")
-		go d.reddit.Listen(shutdown)
-		d.reddit.Log("worker ready")
-
-		<-shutdown
-
-		d.spotify.Close()
-		d.reddit.Close()
-
-		if err := d.http.Shutdown(ctx); err != nil {
-			fmt.Printf("error shutting down http server: %s", err.Error())
-		}
-
-		if d.config.Verbose {
-			log.Println("bye, bye!")
-		}
-	}(ctx, d)
+	// Run dissic client
+	d.Run(ctx)
 }
---
+++ dissic/client.go
@@ -0,0 +1,70 @@
+package dissic
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/engvik/dissic/config"
+	"github.com/engvik/dissic/reddit"
+	"github.com/engvik/dissic/spotify"
+)
+
+type Client struct {
+	Config  *config.Config
+	Spotify *spotify.Client
+	Reddit  *reddit.Client
+	HTTP    *http.Server
+}
+
+func (c *Client) Run(ctx context.Context) {
+	go func(s *http.Server) {
+		if err := s.ListenAndServe(); err != nil {
+			log.Fatalf("error starting http server: %s", err.Error())
+		}
+	}(c.HTTP)
+
+	// Authenticate spotify
+	c.Spotify.Log("awaiting authentication...")
+	c.Spotify.Authenticate()
+	<-c.Spotify.AuthChan
+	c.Spotify.Log("authenticated!")
+
+	// Get Spotify playlists
+	if err := c.Spotify.GetPlaylists(c.Config); err != nil {
+		log.Fatalf("error preparing playlists: %s", err.Error())
+	}
+
+	// Prepare the reddit scanner
+	if err := c.Reddit.PrepareScanner(); err != nil {
+		log.Fatalf("error preparing reddit/graw scanner: %s", err.Error())
+	}
+
+	// Start listening and block until shutdown signal receieved
+	func(ctx context.Context, c *Client) {
+		shutdown := make(chan os.Signal, 1)
+		signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+		go c.Spotify.Listen()
+		c.Spotify.Log("worker ready")
+		go c.Reddit.Listen(shutdown)
+		c.Reddit.Log("worker ready")
+
+		<-shutdown
+
+		c.Spotify.Close()
+		c.Reddit.Close()
+
+		if err := c.HTTP.Shutdown(ctx); err != nil {
+			fmt.Printf("error shutting down http server: %s", err.Error())
+		}
+
+		if c.Config.Verbose {
+			log.Println("bye, bye!")
+		}
+	}(ctx, c)
+}
