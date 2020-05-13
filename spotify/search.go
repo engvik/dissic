89c8ee8cb041797a738b1a8347ca0b39d0393cd3
Lars Jansøn Engvik <lars@engvik.nu>
2020-05-13 22:18:50 +0200

    feature(spotify): slightly better search
    Still needs proper refactoring and improvement, but helps #7 a bit along
    the way.

--- spotify/search.go
+++ spotify/search.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/zmb3/spotify"
 )
@@ -12,59 +13,55 @@ import (
 func (c *Client) getTrack(title string) (spotify.FullTrack, error) {
 	var track spotify.FullTrack
 
-	sq, err := c.createSearchQuery(title)
-	if err != nil {
-		return track, fmt.Errorf("search query: %w", err)
-	}
+	separators := []string{"-", "~", "|", "by", "--"}
 
-	c.Log(fmt.Sprintf("\tsearch query: %s from title: %s", sq, title))
+	for _, s := range separators {
+		searchQuery, err := c.createSearchQuery(title, s)
+		if err != nil {
+			return track, fmt.Errorf("search query: %w", err)
+		}
 
-	res, err := c.search(sq)
-	if err != nil {
-		return track, fmt.Errorf("searching: %w", err)
-	}
+		res, err := c.search(searchQuery)
+		if err != nil {
+			return track, fmt.Errorf("searching: %w", err)
+		}
 
-	cmprTitle := strings.ToLower(title)
+		cmprTitle := strings.ToLower(title)
 
-	for _, t := range res.Tracks.Tracks {
-		if strings.Contains(cmprTitle, strings.ToLower(t.Name)) {
-			for _, artist := range t.Artists {
-				if strings.Contains(cmprTitle, strings.ToLower(artist.Name)) { // TODO attempt replacing & with and in title
-					c.Log(fmt.Sprintf("\ttrack found: %s (%s)", title, t.ID))
-					return t, nil
+		for _, t := range res.Tracks.Tracks {
+			if strings.Contains(cmprTitle, strings.ToLower(t.Name)) {
+				for _, artist := range t.Artists {
+					if strings.Contains(cmprTitle, strings.ToLower(artist.Name)) { // TODO attempt replacing & with and in title
+						c.Log(fmt.Sprintf("\ttrack found: %s (%s)", title, t.ID))
+						return t, nil
+					}
 				}
 			}
 		}
+
+		time.Sleep(1 * time.Second) // TODO: Handle better with workers for entire chan
 	}
 
 	return track, errors.New(fmt.Sprintf("no track found: %s", title))
 }
 
-func (c *Client) createSearchQuery(t string) (string, error) {
+func (c *Client) createSearchQuery(title string, separator string) (string, error) {
 	re := regexp.MustCompile(`\(([^)]+)\)|\[([^)]+)\]`)
-	replacedTitle := re.ReplaceAll([]byte(t), []byte(""))
-	title := string(replacedTitle)
-	title = strings.ReplaceAll(title, "'", "")
-	title = strings.ReplaceAll(title, "\"", "")
+	replacedTitle := re.ReplaceAll([]byte(title), []byte(""))
+	searchQuery := string(replacedTitle)
+	searchQuery = strings.ReplaceAll(searchQuery, "'", "")
+	searchQuery = strings.ReplaceAll(searchQuery, "\"", "")
 
-	var splitTitle []string
-	separators := []string{"-", "~", "|", "by", "--"}
-
-	for _, s := range separators {
-		splitTitle = strings.Split(title, fmt.Sprintf(" %s ", s))
-
-		if len(splitTitle) >= 1 {
-			break
-		}
-	}
+	splitTitle := strings.Split(searchQuery, fmt.Sprintf(" %s ", separator))
 
 	if len(splitTitle) <= 1 {
-		return "", errors.New(fmt.Sprintf("not able to find title and/or artist: %s", title))
+		return "", errors.New(fmt.Sprintf("not able to find title and/or artist: %s", searchQuery))
 	}
 
-	title = strings.Join(splitTitle, " ")
-	title = strings.TrimSpace(title)
+	searchQuery = strings.Join(splitTitle, " ")
+	searchQuery = strings.TrimSpace(searchQuery)
 
+	c.Log(fmt.Sprintf("\tgot search query: %s from title: %s", searchQuery, title))
 	return title, nil
 }
 
